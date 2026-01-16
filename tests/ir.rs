8e5b5e3f391eedd2bdc0ff0b6e2dc2677ea35462
lZh1r <electroxhackerpig09@gmail.com>
2026-01-16 15:46:19 +0300

    put all the tests running on it::Ast to ir.rs

--- tests/ir.rs
+++ tests/ir.rs
@@ -89,3 +89,30 @@ fn test_fun_shadowing() {
         _ => panic!()
     };
 }
+
+#[test]
+fn test_whitespace_max() {
+    let ast = parse_code(r#"
+        let a = 12 ; 
+        let mega_function = fun ( x ) {
+            a + x
+        } ;
+        let b = { bb : 2 } ;
+        mega_function ( b . bb ) ;
+    "#);
+    
+    match run(&ast).unwrap() {
+        Value::Int(i) => assert_eq!(i, 14),
+        _ => panic!()
+    };
+}
+
+#[test]
+fn test_whitespace_min() {
+    let ast = parse_code(r#"let a=12;let mega_function=fun(x){a+x};let b={bb:2};mega_function(b.bb);"#);
+    
+    match run(&ast).unwrap() {
+        Value::Int(i) => assert_eq!(i, 14),
+        _ => panic!()
+    };
+}
